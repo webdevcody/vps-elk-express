f595d9d17ef93013c32bab64189438dc5becac89
Web Dev Cody <webdevcody@gmail.com>
2023-09-26 17:08:42 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const winston = require("winston");
+const expressWinston = require("express-winston");
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false;
+    }, // optional: allows to skip some log messages based on request and/or response
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("gg");
+});
+
+app.get("/hello", (req, res) => {
+  res.status(500);
+  res.send("hello world");
+});
+
+app.get("/bye", (req, res) => {
+  res.send("bye world");
+});
+
+app.listen(3000);
